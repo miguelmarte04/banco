d4d01c631429a9de3b81100e5f1011333eb72697
Miguel <mjml0412@gmail.com>
2022-07-19 00:38:34 -0400

    agregando nombre a las tarjetas

--- inicio.php
+++ inicio.php
@@ -26,38 +26,45 @@ if(isset($_SESSION['usuario'])){
 <div class="contenedor2">
     <div class="card">
         <div>
-            
-            <h2><a href="paquetes_registrados.php">Registrados para enviar</a></h2>
-            <h4><a href="paquetes_registrados.php"><?php echo $para_enviar;?></a></h4>
+        <br>
+        <a href="paquetes_registrados.php"><h2>Solicitud de Tarjeta</h2></a>
+          <br>  
         </div>
     </div>
     <div class="card">
         <div>
             
-            <h2><a href="paquetes_enviados.php">Paquetes enviados</a></h2>
-            <h4><a href="paquetes_enviados.php"><?php echo $enviados;?></a></h4>
+        <br>
+        <a href="paquetes_registrados.php"><h2>Realizar Pagos</h2></a>
+          <br>  
+          
         </div>
     </div>
     <div class="card">
         <div>
             
-            <h2><a href="paquetes_disponibles.php">Paquetes disponibles</a></h2>
-            <h4><a href="paquetes_disponibles.php"><?php echo $disponibles;?></a></h4>
+        <br>
+        <a href="paquetes_registrados.php"><h2>Solicitar Préstamo</h2></a>
+          <br>  
         </div>
     </div>
 
-    <div class="card">
-        <div>
-            
-            <h2><a href="paquetes_entregados.php">Paquetes entregados</a></h2>
-            <h4><a href="paquetes_entregados.php"><?php echo $entregados;?></a></h4>
-        </div>
-    </div>   
+   
+  
 </div>
 <div class="img_portada">
     <img src="img/logoPrincipal.png" alt="portada" >
 </div>
 
+<style>
+    a{
+        text-decoration: none;
+        color: black;
+    }
+    a:hover{
+        color: white;
+    }
+</style>
 
     
 </body>
